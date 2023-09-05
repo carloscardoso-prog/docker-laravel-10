d757454b6fe219d127503607c153ed0cdf4b64c5
carloscardoso-prog <carloseduardocardsilva@gmail.com>
2023-09-04 22:43:04 -0300

    paginaçao e icones

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutosController extends Controller
 {
     public function index(){
-        return 'produto';
+        $listarProduto = Produto::all($columns = ['*']);
+        //$listarAtivos = Produto::where('situacao_id', '!=', '999');
+        // dd($listarProduto);
+        return view('pages.produtos.paginacao', compact('listarProduto'));
     }
 }
--- database/seeders/ProdutosSeeder.php
+++ database/seeders/ProdutosSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Produto;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,11 @@ class ProdutosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Produto::create([
+            
+            'nome_produto' => 'Carro Bonito',
+            'tipo_produto' => 'Veículo Automotivo',
+            'valor' => '50000.00',
+        ]);
     }
 }
